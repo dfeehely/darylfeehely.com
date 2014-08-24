f1c2a065b5031ac0a069880e98ade3e6b849397f
dfeehely <dfeehely@gmail.com>
2014-08-24 20:17:43 +0100

    bsic homepage

--- js/responsivegridsystem.js
+++ js/responsivegridsystem.js
@@ -24,3 +24,58 @@ months[10] = "November";
 months[11] = "December";
 
 // end script hiding -->
+
+$(document).ready(function() {  
+	var stickyNavTop = $('#menu').offset().top;  
+	var stickyNav = function(){  
+		var scrollTop = $(window).scrollTop();  
+		       
+		if (scrollTop > stickyNavTop) {   
+		    $('#menu').addClass('sticky');  
+		} else {  
+		    $('#menu').removeClass('sticky');   
+		}  
+	};  
+	  
+	stickyNav();  
+	  
+	$(window).scroll(function() {  
+	    stickyNav();  
+	});  
+});  
+
+$(function () {
+	var filterList = {
+		init: function () {
+		
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixitup({
+				targetSelector: '.portfolio',
+				filterSelector: '.filter',
+				effects: ['fade'],
+				easing: 'snap',
+				// call the hover effect
+				onMixEnd: filterList.hoverEffect()
+			});				
+		
+		},
+		hoverEffect: function () {
+			// Simple parallax effect
+			$('#portfoliolist .portfolio').hover(
+				function () {
+					$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+					$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+				},
+				function () {
+					$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+					$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+				}		
+			);				
+		
+		}
+
+	};
+	// Run the show!
+	filterList.init();
+});	
