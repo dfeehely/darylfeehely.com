3d0d51a099139a0c14f95b9d400810f20ee6293d
dfeehely <dfeehely@gmail.com>
2014-08-26 22:47:35 +0100

    work cards

--- js/responsivegridsystem.js
+++ js/responsivegridsystem.js
@@ -25,23 +25,9 @@ months[11] = "December";
 
 // end script hiding -->
 
+
 $(document).ready(function() {  
-	var stickyNavTop = $('#menu').offset().top;  
-	var stickyNav = function(){  
-		var scrollTop = $(window).scrollTop();  
-		       
-		if (scrollTop > stickyNavTop) {   
-		    $('#menu').addClass('sticky');  
-		} else {  
-		    $('#menu').removeClass('sticky');   
-		}  
-	};  
-	  
-	stickyNav();  
-	  
-	$(window).scroll(function() {  
-	    stickyNav();  
-	});  
+
 });  
 
 $(function () {
